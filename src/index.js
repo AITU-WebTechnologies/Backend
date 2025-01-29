054e0035f9dbefd572d790dfcfbb89052842f428
JFN <eserikova22@gmail.com>
2025-01-29 17:07:16 +0500

    feat: last changes

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: '37.151.225.71:4000',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
